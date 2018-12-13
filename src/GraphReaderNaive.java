60e464d138a753a1f3e568577a8e2b5791b63ac1
intern@cleverrobotlabs.com <comte.florian@gmail.com>
2018-12-13 23:10:53 +0100

    forgot to push

--- src/GraphReaderNaive.java
+++ src/GraphReaderNaive.java
@@ -1,20 +1,32 @@
+import java.awt.List;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class GraphReaderNaive {
+	public static String stdOrCustom;
+	public static String weightedOrNot;
+	public static int nbrElements;
 	// TODO !
 	public static void main(String[] args) {
-		//final String verticesFilename = args[0];
-		//final String edgesFilename = args[1];
+		stdOrCustom = args.length > 0 ? args[0] : "std";
+		weightedOrNot = args.length > 1 ? args[1] : "notWeighted";
+		nbrElements = args.length > 2 ? Integer.parseInt(args[2]) : Integer.MAX_VALUE;
+		
+		System.out.println("Std or custom ? " + stdOrCustom);
+		System.out.println("Weighted or not ? " + weightedOrNot);
+		
 		try {
-			//System.out.println(args[0]);
 			Node[] nodes= readGraph("./data/paris-vertices.txt", "./data/paris-edges.txt");
 			System.err.println("returned " + nodes.length + " elts.");
 			Plotter p = new Plotter(nodes);
+			Date begin = new Date();
 			p.plot(nodes);
+			System.out.println("Took " + (new Date().getTime() - begin.getTime())/1000f + " sec to read");
+			
 		} catch (FileNotFoundException e) {
 			System.err.println(e);
 		}
@@ -22,7 +34,13 @@ public class GraphReaderNaive {
 	
 	private static Node[] readGraph(String verticesFileName, String edgesFileName) throws FileNotFoundException {
 		
-		SingleLinkedList<Node> nodes = new SingleLinkedList<Node>();
+		int i = 0;
+		
+		Date begin = new Date();
+		
+		java.util.List<Node> nodes;
+		
+		nodes = GraphReaderNaive.stdOrCustom.compareTo("std") == 0 ? new ArrayList<Node>() : new SingleLinkedList<Node>();
 		
 		try {
 			File verticesFile = new File(verticesFileName);
@@ -31,33 +49,38 @@ public class GraphReaderNaive {
 			Scanner verticeScanner = new Scanner(verticesFile);
 			Scanner edgeScanner = new Scanner(edgesFile);
 			
-			while (verticeScanner.hasNextLine()) {
+			while (verticeScanner.hasNextLine() && i < GraphReaderNaive.nbrElements) {
 				String line = verticeScanner.nextLine();
 				String[] points = line.split(" ");
 				Node n = new Node(Double.parseDouble(points[0]), Double.parseDouble(points[1]));
-				nodes.add(n); 
+				nodes.add(n);
+				i++;
 			}
-			
-			System.out.println(nodes.size());
-						
+									
 			while (edgeScanner.hasNextLine()) {
 				String line = edgeScanner.nextLine();
 				String[] data = line.split(" ");
-				int fromIndex = nodes.size() - 2  - Integer.parseInt(data[0]);
-				int toIndex = nodes.size() - 2 - Integer.parseInt(data[1]);
+				
+				int fromIndex = GraphReaderNaive.stdOrCustom.compareTo("std") == 0 ? Integer.parseInt(data[0]) : nodes.size() - 2 - Integer.parseInt(data[0]);
+				int toIndex = GraphReaderNaive.stdOrCustom.compareTo("std") == 0 ? Integer.parseInt(data[1]) : nodes.size() - 2 - Integer.parseInt(data[1]);
+
+				if (fromIndex > GraphReaderNaive.nbrElements - 1 || toIndex > GraphReaderNaive.nbrElements - 1)
+					continue;
+				
 				boolean bothDir = Integer.parseInt(data[2]) == 2;
-				//int distance = Integer.parseInt(data[3]);
-				System.out.println(fromIndex + "   " + toIndex);
-				nodes.get(fromIndex).addNeighbor(nodes.get(toIndex));
+				int distance = Integer.parseInt(data[3]);
+				//System.out.println(fromIndex + "   " + toIndex);
+				nodes.get(fromIndex).addNeighbor(nodes.get(toIndex), distance);
 				if (bothDir) {
-					nodes.get(toIndex).addNeighbor(nodes.get(fromIndex));
-				}			
+					nodes.get(toIndex).addNeighbor(nodes.get(fromIndex), distance);
+				}
 			}
-			
 			
 		} catch (FileNotFoundException e) {
 			throw e;
 		}
+		
+		System.out.println("Took " + (new Date().getTime() - begin.getTime())/1000f + " sec to write");
 		
 		return nodes.toArray(new Node[nodes.size()]);
 
--- src/Node.java
+++ src/Node.java
@@ -1,12 +1,16 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
-class Node {
+class Node implements Comparable {
 	public final double lonRadians;
 	public final double latRadians;
-	private List<Node> neighbors;
+	//private List<Node> neighbors;
+	private Object neighbors;
 
 	private static double toRadians(double degrees) {
 		return degrees * Math.PI / 180;
@@ -15,15 +19,41 @@ class Node {
 	public Node(double lon, double lat) { // (node-cstor)
 		lonRadians = toRadians(lon);
 		latRadians = toRadians(lat);
-		neighbors = new SingleLinkedList<Node>();
+		
+		if (GraphReaderNaive.weightedOrNot.compareTo("notWeighted") == 0) {
+			
+			neighbors = GraphReaderNaive.stdOrCustom.compareTo("std") == 0 ? new ArrayList<Node>() : new SingleLinkedList<Node>();
+		}
+		
+		else {
+			neighbors = GraphReaderNaive.stdOrCustom.compareTo("std") == 0 ? new TreeMap<Node, Integer>() : new ListMap<Node, Integer>();
+		}
+		
 	}
 
-	public void addNeighbor(Node other) {
-		neighbors.add(other);
+	public void addNeighbor(Node other, int distance) {
+		if (GraphReaderNaive.weightedOrNot.compareTo("notWeighted") == 0) {
+			((List)neighbors).add(other);
+		}
+		else {
+			((Map)neighbors).put(other, distance);
+		}
 	}
 
 	public Node[] getNeighbors() {
-		return neighbors.toArray(new Node[neighbors.size()]);
+				
+		if (GraphReaderNaive.weightedOrNot.compareTo("notWeighted") == 0) {
+			return ((List<Node>)neighbors).toArray(new Node[((List)neighbors).size()]);
+		}
+		else {
+			return (Node[]) ((Map<Node, Integer>)neighbors).keySet().toArray(new Node[((Map)neighbors).size()]);
+		}
+		
+	}
+
+	@Override
+	public int compareTo(Object other) {
+		return this.equals(other) ? 0 : 1;
 	}
 }
 
